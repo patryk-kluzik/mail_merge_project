40d068ddf9879b9a863f9161a757aa0ef9e6e1c2
patryk-kluzik <patrykkluzik99@gmail.com>
2022-03-28 17:19:38 +0100

    need to fix joining string and allow for files to be created

--- main.py
+++ main.py
@@ -7,4 +7,28 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+with open(file="./Input/Names/invited_names.txt") as names:
+    all_names = []
+    for name in names.readlines():
+        all_names.append(name.strip())
 
+
+
+with open(file="./Input/Letters/starting_letter.txt") as letter:
+    first_line = letter.readline()
+    remaining_lines = []
+    for line in  letter.readlines()[1:]:
+        if line == "\n":
+            remaining_lines.append(line)
+        else:
+            remaining_lines.append(line.strip())
+    for name in all_names:
+        full_text = first_line.join(remaining_lines).replace("[name]", name)
+        #
+        # with open(file=f"./Output/ReadyToSend/letter_for_{name}.txt", mode="x") as output:
+        #     output.write(first_line)
+        #     output.write(remaining_lines)
+
+
+
+print(full_text)

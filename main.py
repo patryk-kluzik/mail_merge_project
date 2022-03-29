146745a12ac6a8564a9c129dc39fc8994275e8f3
patryk-kluzik <patrykkluzik99@gmail.com>
2022-03-29 09:04:06 +0100

    fixed and completed

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = '[name]'
 
 with open(file="./Input/Names/invited_names.txt") as names:
     all_names = []
     for name in names.readlines():
         all_names.append(name.strip())
 
-
-
 with open(file="./Input/Letters/starting_letter.txt") as letter:
-    first_line = letter.readline()
-    remaining_lines = []
-    for line in  letter.readlines()[1:]:
-        if line == "\n":
-            remaining_lines.append(line)
-        else:
-            remaining_lines.append(line.strip())
+    all_lines = letter.read()
+
     for name in all_names:
-        full_text = first_line.join(remaining_lines).replace("[name]", name)
-        #
-        # with open(file=f"./Output/ReadyToSend/letter_for_{name}.txt", mode="x") as output:
-        #     output.write(first_line)
-        #     output.write(remaining_lines)
+        full_text = ''.join(all_lines).replace(PLACEHOLDER, name)
 
+        with open(file=f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as output:
+            output.write(full_text)
 
-
-print(full_text)
+print(all_lines)
